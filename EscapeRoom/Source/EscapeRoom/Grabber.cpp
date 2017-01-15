cfa6920d91de535bdac3bc1bcdbacbc12ae05774
PanayiotisPh <pr.filippou@gmail.com>
2017-01-15 15:17:24 +0200

    08 Player View Point Print Out

---
+++ EscapeRoom/Source/EscapeRoom/Grabber.cpp
@@ -0,0 +1,42 @@
+// Copyright Panayiotis Philippou 2017.
+
+#include "EscapeRoom.h"
+#include "Grabber.h"
+#define OUT
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Error, TEXT("Grabber Ready!"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// Get player View point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation,OUT PlayerViewPointRotation);
+	// TODO logout to test
+	UE_LOG(LogTemp, Warning, TEXT("Players View Point is: Location %s, Position %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString())
+
+	// Ray-casting out to reach distance
+	// See what we hit
+}
+
