e853faf875eec7272099029dd3c5982b67bc7316
PanayiotisPh <pr.filippou@gmail.com>
2017-01-17 10:29:39 +0200

    10 LineTraceSingleBy Object Type, find and print out what object is in our reach

--- EscapeRoom/Source/EscapeRoom/Grabber.cpp
+++ EscapeRoom/Source/EscapeRoom/Grabber.cpp
@@ -42,7 +42,7 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 
 	DrawDebugLine(
 		GetWorld(),
-		PlayerViewPointLocation,
+		(PlayerViewPointLocation),
 		LineTraceEnd,
 		FColor(127, 255, 0),
 		false,
@@ -50,8 +50,23 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 		0.f,
 		5.f
 	);
+	//Setup Query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
 
 	// Ray-casting out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
 	// See what we hit
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), (*ActorHit->GetName()))
+	}
 }
 
